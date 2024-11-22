b43949ffaa05bfd213b49360c30adb3fc43cb3d5
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-22 19:37:28 +0100

    Ajout de la configuration de Tailwind CSS : couleurs, tailles de police et polices de caractères pour améliorer le style du projet"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      text: "#050606",
+      background: "#f6f8f9",
+      primary: "#699aab",
+      secondary: "#a3c7d1",
+      accent: "#71aec1",
+    },
+    fontSize: {
+      sm: "0.750rem",
+      base: "1rem",
+      xl: "1.333rem",
+      "2xl": "1.777rem",
+      "3xl": "2.369rem",
+      "4xl": "3.158rem",
+      "5xl": "4.210rem",
+    },
+    fontFamily: {
+      heading: "Noto Serif NP Hmong",
+      body: "Noto Serif NP Hmong",
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+    },
     extend: {},
   },
   plugins: [],
