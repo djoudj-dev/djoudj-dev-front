e16938db762bc8836ab946754702df544b8f91f6
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-23 16:58:36 +0100

    Ajouter une interface MENU_ITEMS pour la barre de navigation ; refactoriser pour utiliser des définitions centralisées des éléments de menu afin d'améliorer la maintenabilité.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
+export interface MenuItem {
+  route: string;
+  label: string;
+}
+
+export const MENU_ITEMS: MenuItem[] = [
+  { route: '/about', label: 'À propos' },
+  { route: '/projects', label: 'Projets' },
+  { route: '/stack', label: 'Stacks' },
+  { route: '/contact', label: 'Contact' },
+];
+
 export const routes: Routes = [];
